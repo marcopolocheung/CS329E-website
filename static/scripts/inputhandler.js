92b4f80d988747cdef715725cd5a258408617714
marcopolocheung <syy6x10yh69ueawn7dwesxhzjxlpfh@bots.bitbucket.org>
2025-11-10 15:40:35 -0600

    11/10 ass 4

--- static/scripts/inputhandler.js
+++ static/scripts/inputhandler.js
@@ -1,13 +1,13 @@
+// city = name of the city
+// data_label = label that goes with the data
+// xaxis_data = number of days
+// chart_data = the actual values to plot (temperature, precipitation, snow)
 function draw_chart(city, data_label, xaxis_data, chart_data) {
 
-	outputImg = document.getElementById("outputImg");
-	// TODO: delete previous children
-	//outputImg.innerHTML = '';
-	newElem = document.createElement("canvas");
-	elemId = city + "-" + data_label;
-	newElem.setAttribute("id",elemId);
-
-	//const ctx = document.getElementById('myChart');
+  outputImg = document.getElementById("outputImg");
+  newElem = document.createElement("canvas");
+  elemId = city + "-" + data_label;
+  newElem.setAttribute("id",elemId);
 
   new Chart(newElem, {
     type: 'line',
@@ -17,15 +17,11 @@ function draw_chart(city, data_label, xaxis_data, chart_data) {
         label: data_label,
 	fill: false,
         data: chart_data,
-	//fill: false,
-	//borderColor: 'rgb(75, 192, 192)',
-	//tension: 0.1,
         borderWidth: 1
       }]
     },
     options: {
       responsive: true,
-      //maintainAspectRatio: false,
       scales: {
         y: {
 	  display: true,
@@ -46,68 +42,7 @@ function draw_chart(city, data_label, xaxis_data, chart_data) {
     }
   });
 
-outputImg.appendChild(newElem);
-}
-
-function draw_chart_orig() {
-const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-}
-
-function validate() {
-	ids = ["e_we","e_sn","e_vert","ref_lat","ref_lon","truelat1","truelat2"]
-	empty_ids = []
-	for (i=0; i<ids.length; i++) {
-	    val = document.getElementById(ids[i]).value;
-	    if (isNaN(val)) {
-		empty_ids.push(ids[i]);
-	    }
-	}
-	if (empty_ids.length > 0) {
-		alert("Should be a number:" + empty_ids.join(", "))
-		return false;
-	}
-
-	// Check format of start_date and end_date
-	// YYYY-MM-DD_HH:MM:SS
-	var regEx = /^\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2}$/;
-	start_date = document.getElementById("start_date").value;
-	end_date = document.getElementById("end_date").value;
-	s_format = start_date.match(regEx);
-	e_format = end_date.match(regEx);
-
-	if (!s_format || !e_format) {
-		incorrect_format = " incorrect format.";
-		who = "";
-		if (!s_format) {
-			who = "start_date";
-		}
-		if (!e_format) {
-			who = who + " end_date";
-		}
-		stat = who + " " + incorrect_format;
-		alert(stat)
-		return false;
-	}
-	return true; 
+  outputImg.appendChild(newElem);
 }
 
 function get_city_status(city) {
@@ -115,8 +50,6 @@ function get_city_status(city) {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          //Reference: https://stackoverflow.com/questions/33914245/how-to-replace-the-entire-html-webpage-with-ajax-response
-
          fieldData = JSON.parse(this.responseText);
          console.log(fieldData);
          console.log("-------");
@@ -185,63 +118,45 @@ function get_city_status(city) {
       xhttp.send();
 }
 
-function get_city_status_graph(city) {
-	//alert(runRef.id)
+
+// TODO: Assignment 4 show the graph
+function get_city_weather_graphs(city) {
 
       var xhttp;
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          //Reference: https://stackoverflow.com/questions/33914245/how-to-replace-the-entire-html-webpage-with-ajax-response
 
          fieldData = JSON.parse(this.responseText);
          console.log(fieldData);
          console.log("-------");
 
-	/*
-	 imageData = fieldData["imageData"];
-	 
-	 var currentImg = document.getElementById("output-img");
-	 if (currentImg != null) {
-	 	currentImg.parentNode.removeChild(currentImg);
-	 }
-	
-	 statusString = document.getElementById("status_string");
-	 statusString.innerHTML = "Output of " + runRef.id;
-	 //statusString.setAttribute("style", "display:none;");
-
-	 imgElem = document.createElement("img");
-	 srcVal = "data:image/png;base64," + imageData; 
-	 imgElem.setAttribute("src", srcVal);
-	 imgElem.setAttribute("id", "output-img");
-	 
-	 document.getElementById("outputImg").appendChild(imgElem);*/
 	 output_string = "";
 	 statusString = document.getElementById("status_string");
 	 statusString.innerHTML = "Output of " + city.id;
 	 // Clear the previous canvas elements;
 	 outputImg = document.getElementById("outputImg");
 	 outputImg.innerHTML = '';
+
+	 // Example fieldData: 
+         // {'2023-08-TMAX': '411,406,400,406,411,406,411,411,422,417,422,411,411,411,389,400,433,411,400,417,-9999,-9999,-9999', '2023-08-TMIN': '256,261,256,261,256,256,256,256,250,256,261,267,261,250,267,228,250,250,239,256,-9999,-9999,-9999', '2023-08-PRCP': '-9999,-9999,-9999'}
+	 // Iterate through the <key, value> pairs and call draw_chart for each 
 	 for (const [key, value] of Object.entries(fieldData)) {
   		console.log(`${key}: ${value}`);
 		vals = value.split(",");
 		values = [];
 		xaxis_vals = [];
 		count = 1;
-		for(const elem of vals) {
-			if (elem != "-9999") {
-				// Convert the number to decimal
-				parts = elem.split("");
-				elem1 = parts.slice(0, parts.length-1);
-				elem1.push(".");
-				elem1.push(parts[parts.length-1]);
-				elem2 = elem1.join("");
-				values.push(elem2);
-				// Xaxis Day counter.
+
+		//#######4DONE####### Assignment 4 Iterate through vals; ignore entries that are "-9999"; convert to decimal (411 -> 41.1)
+		for (var i = 0; i < vals.length; i++) {
+			if (vals[i] !== "-9999") {
+				values.push(vals[i] / 10);
 				xaxis_vals.push(count);
-				count = count + 1;
+				count++;
 			}
 		}
+
 		key1 = key;
 		if (key.includes("TMAX") || key.includes("TMIN")) {
 			key1 = key1 + " (Celcius)"
@@ -259,7 +174,7 @@ function get_city_status_graph(city) {
         }
       };
 
-      url = "/status?city=" + city.id;
+      url = "/weather_params?city=" + city.id;
       xhttp.open("GET", url, true);
       xhttp.send();
 }
