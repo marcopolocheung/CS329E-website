1f78c5d8133339a94d1b2b8696ba75fb34c784a1
marcopolocheung <syy6x10yh69ueawn7dwesxhzjxlpfh@bots.bitbucket.org>
2025-11-29 20:25:50 -0600

    project5

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Response
 
 import requests
 
+import flask
 from flask import request
 from flask import Flask, render_template
 
@@ -14,11 +15,30 @@ import json
 import os
 
 import time
+import bcrypt
 
 
 from flask import session
 
 from multiprocessing import Process
+
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "google_oauth_client_secret.json"
+
+
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email']
+API_SERVICE_NAME = 'oauth2'
+API_VERSION = 'v2'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 
 app = Flask(__name__)
@@ -93,12 +113,13 @@ class Admin(Base):
         return "<Admin(name='%s')>" % (self.name)
 
     # Ref: https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+    # 1. Assignment 5: Modify the method to not return password in the returned fields
     def as_dict(self):
         fields = {}
         for c in self.__table__.columns:
-            fields[c.name] = getattr(self, c.name)
+            if c.name != 'password':
+                fields[c.name] = getattr(self, c.name)
         return fields
-        #return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class User(Base):
@@ -111,13 +132,13 @@ class User(Base):
         return "<User(name='%s')>" % (self.name)
 
     # Ref: https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+    # 2. Assignment 5: Modify the method to not return password in the returned fields
     def as_dict(self):
         fields = {}
         for c in self.__table__.columns:
-            #if c.name != 'password':
-            fields[c.name] = getattr(self, c.name)
+            if c.name != 'password':
+                fields[c.name] = getattr(self, c.name)
         return fields
-        #return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class City(Base):
@@ -198,12 +219,7 @@ class ETL():
             city_name = city.name
             city_url = city.url
             r = requests.get(city_url)
-
-            #######4DONE####### 1. Assignment 4 TODO: Check if the city has data available by checking for return code 404
-            if r.status_code == 404:
-                app.logger.info(f"No weather data for {city_name}. STATUSCODE 404!!!")
-                continue
-
+            # 1. Assignment 4 TODO: Check if the city has data available by checking for return code 404
             city_data = r.text
             data_dir = os.getcwd() + "/data"
             os.system("mkdir -p " + data_dir)
@@ -268,6 +284,29 @@ p = Process(target=etl.run)
 p.start()
 
 
+# https://stackoverflow.com/questions/19473250/how-to-get-user-email-after-oauth-with-google-api-python-client
+def get_user_info(credentials):
+    user_info_service = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    user_info = None
+    try:
+        user_info = user_info_service.userinfo().get().execute()
+    except errors.HttpError as e:
+        logging.error('An error occurred: %s', e)
+    if user_info and user_info.get('id'):
+        return user_info
+    else:
+        raise NoUserIdException()
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+
 
 ## Admin REST API
 @app.route("/admin", methods=['POST'])
@@ -279,6 +318,7 @@ def add_admin():
     name = data['name']
     password = data['password']
 
+
     session = DBSession()
     admin = session.query(Admin).filter_by(name=name).first()
 
@@ -286,7 +326,10 @@ def add_admin():
         status = ("Admin with name {name} already exists.\n").format(name=name)
         return Response(status, status=400)
     else:
-        admin = Admin(name=name, password=password)
+        # 3. Assignment 5: Use bcrypt to encrypt the password.
+        
+        bcryptPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        admin = Admin(name=name, password=bcryptPassword.decode('utf-8'))
         session.add(admin)
         session.commit()
 
@@ -351,7 +394,9 @@ def add_user():
     name = data['name']
     password = data['password']
 
-    newuser = User(name=name, password=password)
+    # 4. Assignment 5: Use bcrypt to encrypt the password.
+    bcryptPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    newuser = User(name=name, password=bcryptPassword.decode('utf-8'))
 
     session = DBSession()
     user = session.query(User).filter_by(name=name).first()
@@ -430,6 +475,7 @@ def add_city(admin_id):
     else:
         city = City(name=name, url=url, adminId=admin.id)
 
+        # 2. Assignment 4: Check if City with name already exists; Don't add if it already exists
         existing_city = session.query(City).filter_by(name=name).first()
         if existing_city != None:
             status = ("City with name {name} already exists.\n").format(name=name)
@@ -554,20 +600,16 @@ def get_user_cities(dbsession, userid):
             cities.append(city)
     return cities
 
-#######4DONE####### 2. Assignment 4 
-# Query cities registered by admin 
-# Return the cities list where every entry will be a city dict of the form [city{'name'}:'Ausitn', city{'name'}: 'Dallas']
-def get_admin_cities(dbsession):
 
+def get_admin_cities(dbsession):
     cities = []
-    all_cities = dbsession.query(City).all()
-    
-    for cty in all_cities:
+    admin_cities = dbsession.query(City)
+    for cty in admin_cities:
         city = {}
         city['name'] = cty.name
         cities.append(city)
+    return cities 
 
-    return cities
 
 @app.route("/status", methods=['GET'])
 def city_status():
@@ -601,12 +643,13 @@ def city_status():
     return json.dumps(op)
 
 
-#######4DONE####### 3. Assignment 4 
+## 3. Assignment 4 
 ## Expected Output:
 ### {'2023-08-TMAX': '411,406,400,406,411,406,411,411,422,417,422,411,411,411,389,400,433,411,400,417,-9999,-9999,-9999', '2023-08-TMIN': '256,261,256,261,256,256,256,256,250,256,261,267,261,250,267,228,250,250,239,256,-9999,-9999,-9999', '2023-08-PRCP': '-9999,-9999,-9999'}
 @app.route("/weather_params", methods=['GET'])
 def city_status_graph():
-    app.logger.info("Inside city_status_graph")
+    op = {}
+    app.logger.info("Inside city_status")
     city_name = request.args.get('city').strip()
     app.logger.info("City status:" + city_name)
     
@@ -616,36 +659,29 @@ def city_status_graph():
 
     app.logger.info("Username:" + username + " City:" + city_name)    
 
-    op = {}
-    
-
     dbsession = DBSession()
+    users = dbsession.query(User).filter_by(name=username)
+    user = users.first()
 
-    user = dbsession.query(User).filter_by(name=username).first()
-    if user == None:
-        return json.dumps(op)
-    city = dbsession.query(City).filter_by(name=city_name).first()
-    if city == None:
-        return json.dumps(op)
-    usercity = dbsession.query(UserCity).filter_by(userId=user.id, cityId=city.id).first()
-    if usercity == None:
-        return json.dumps(op)
-    
-    year = usercity.year
+    cities = dbsession.query(City).filter_by(name=city_name)
+    city = cities.first()
+
+    usercities = dbsession.query(UserCity).filter_by(cityId=city.id,userId=user.id)
+    usercity = usercities.first()
     month = usercity.month
-    params = usercity.weather_params.split(',')
-    
-    for param in params:
+    year = usercity.year
+    weather_params = usercity.weather_params
+    individual_params = weather_params.split(',')
+    for param in individual_params:
+        param = param.strip()
+        year_month_param = year + "-" + month + "-" + param
+        param_values = dbsession.query(WeatherParameter).filter_by(cityId=city.id, year_month_param=year_month_param)
+        if param_values.count() > 0:
+            param_value_str = param_values.first().values
+            op[year_month_param] = param_value_str
+        else:
+            op[year_month_param] = "" 
 
-        key = year + "-" + month + "-" + param
-        weather_param = dbsession.query(WeatherParameter).filter_by(
-            cityId=city.id, 
-            year_month_param=key
-        ).first()
-        
-        if weather_param != None:
-            op[key] = weather_param.values
-    
     app.logger.info(op)
     return json.dumps(op)
 
@@ -727,7 +763,7 @@ def registercity():
                 present = True
                 break
 
-    # 3. Assignment 4 - Return admin_cities
+    # 4. Assignment 4 - Return admin_cities
     admin_cities = get_admin_cities(dbsession)
 
     if present:
@@ -764,38 +800,164 @@ def registercity():
                 status_style="display:block;")
 
 
+# 5. Assignment 5: 
+# - Connect the "login-using-google" form with this method
+# - For http methods list in the definition, use POST and GET
+@app.route("/authorize", methods=['POST', 'GET'])
+def authorize():
+  if not os.path.exists(CLIENT_SECRETS_FILE):
+      return render_template('google-oauth-client-secrets-file-missing.html')
+
+  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+  # The URI created here must exactly match one of the authorized redirect URIs
+  # for the OAuth 2.0 client, which you configured in the API Console. If this
+  # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+  # error.
+  redirect_uri = flask.url_for('oauth2callback', _external=True)
+  parts = redirect_uri.split("/oauth2callback")
+  print(parts)
+  if ":5009" not in redirect_uri:
+    flow.redirect_uri = parts[0] + ":5009/oauth2callback"
+  else:
+    flow.redirect_uri = redirect_uri
+  print(flow.redirect_uri)
+
+  authorization_url, state = flow.authorization_url(
+      # Enable offline access so that you can refresh an access token without
+      # re-prompting the user for permission. Recommended for web server apps.
+      access_type='offline',
+      # Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes='true')
+
+  # Store the state so the callback can verify the auth server response.
+  flask.session['state'] = state
+  print("Authorization URL:" + authorization_url)
+
+  return flask.redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verified in the authorization server response.
+  state = flask.session['state']
+
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = flask.request.url
+  print("Authorization response:" + authorization_response)
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store credentials in the session.
+  # In a production app, you likely want to save these
+  # credentials in a persistent database instead.
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+
+  return flask.redirect(flask.url_for('login'))
+
+
+
 @app.route("/logout",methods=['GET'])
 def logout():
     app.logger.info("Logout called.")
     session.pop('username', None)
     app.logger.info("Before returning...")
+
+    # delete credentials from Flask session
+    if 'credentials' in flask.session:
+        credentials = google.oauth2.credentials.Credentials(**flask.session['credentials'])
+
+        revoke = requests.post('https://oauth2.googleapis.com/revoke',
+                params={'token': credentials.token},
+                headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+        status_code = getattr(revoke, 'status_code')
+        if status_code == 200:
+            print("Credentials successfully revoked.")
+        del flask.session['credentials']
+
     return render_template('index.html')
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST','GET'])
 def login():
-    username = request.form['username'].strip()
-    password = request.form['password'].strip()
+    username = ''
+    password = ''
+    if 'username' in request.form:
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
     app.logger.info("Username:%s", username)
     app.logger.info("Password:%s", password)
+
+    # 5. Assignment 4 - Check if user is present
+    if username != '':
+        dbsession = DBSession()
+        users = dbsession.query(User).filter_by(name=username)
+        if users.count() == 0:
+            return render_template('not-found.html',user=username)
+        else:
+            # 6. Assignment 5:
+            # - Check that <username, password> exists in the database
+            # - Note that password will be encrypted in the DB.
+            # - You will have to use bcrypt's checkpw method to check the password.
+            user = users.first()
+            storedPassword = user.password.encode('utf-8')
+            
+            if not bcrypt.checkpw(password.encode('utf-8'), storedPassword):
+                return render_template('not-found.html', user=username)    
+
+    # Load credentials from the session.
+    if 'credentials' in flask.session:
+        credentials = google.oauth2.credentials.Credentials(
+              **flask.session['credentials'])
+
+        user_email = get_user_info(credentials)['email']
+        print("User email:" + user_email)
+        username = user_email
+
+        # Assignment 5: 
+        # - Insert User in the DB
+        # - Leave password empty
+
+        dbsession = DBSession()
+        existingUser = dbsession.query(User).filter_by(name=username).first()
+        if existingUser is None:
+
+            oauth_user = User(name=username, password='')
+            dbsession.add(oauth_user)
+            dbsession.commit()
+
+        flask.session['credentials'] = credentials_to_dict(credentials)
+
     session['username'] = username
 
-    # 4. Assignment 4 - Check if user is present
-    # If user is not present render_template not-found.html by passing in the username
     dbsession = DBSession()
     users = dbsession.query(User).filter_by(name=username)
-    user_cities = []
-    
-    #######4DONE#######
-    if users.count() == 0:
-        return render_template('not-found.html', username=username)
-    
-    user = users.first()
-    user_cities = get_user_cities(dbsession, user.id)
 
-    # 5. Assignment 4 - Return admin_cities
+    # 6. Assignment 4 - Return admin_cities
     admin_cities = get_admin_cities(dbsession)
 
+    runs = []
+    user_cities = []
+    if users.count() > 0:
+        user = users.first()
+        user_cities = get_user_cities(dbsession, user.id)
+    else:
+        user = User(name=username, password=password)
+        dbsession = DBSession()
+        dbsession.add(user)
+        dbsession.commit()
+
+    my_cities = []
+    if username in in_mem_user_cities:
+        my_cities = in_mem_user_cities[username]
     return render_template('welcome.html',
             welcome_message = "Personal Weather Portal",
             cities=user_cities,
@@ -811,6 +973,8 @@ def login():
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
 
 @app.route("/adminlogin", methods=['POST'])
 def adminlogin():
@@ -841,4 +1005,4 @@ if __name__ == "__main__":
 
     app.debug = False
     app.logger.info('Portal started...')
-    app.run(host='0.0.0.0', port=5009) 
+    app.run(host='0.0.0.0', port=5009, ssl_context='adhoc')
